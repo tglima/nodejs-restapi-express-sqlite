15f56ce69da558f0aab96d897d7f508972e4661b
tglima <thg.limadesousa@gmail.com>
2022-09-19 19:42:33 -0300

    Adiciona as rotas do authRoutes

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const homeRoutes = require('./app/routes/home.routes');
+const authRoutes = require('./app/routes/auth.routes');
 
 app.use(bodyParser.json());
 
 app.use('/api/v1', homeRoutes);
+app.use('/api/v1', authRoutes);
 
 app.listen(4200, () => {
     console.log('Servidor rodando na porta: 4200');
-  });
+});
