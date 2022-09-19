eaa54f4e9fce3a0e789a858cfde9233bc326c6f2
tglima <thg.limadesousa@gmail.com>
2022-09-19 19:33:03 -0300

    Corrigi o path do database.db

--- config/database.config.js
+++ config/database.config.js
@@ -1,8 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('../app/assets/database/database.db', sqlite3.OPEN_READONLY,  (err) => {
+const dbPath = (process.cwd()+'/app/assets/database/database.db');
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
     if(err) {
+        console.error(err);
         console.error('Erro ao conectar o banco de dados!');
+        return;
     } 
     console.log('Banco de dados conectado com sucesso');
 });
