3a605cdc8370c82546bf606a85c1679190cfec7d
tglima <thg.limadesousa@gmail.com>
2022-09-24 15:28:46 -0300

    Realiza ajustes nas configurações

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,33 @@
 module.exports = {
-    'env': {
-        'es2021': true,
-        'node': true
-    },
-    'extends': 'eslint:recommended',
-    'plugin': 'node/recommended',
-    'plugin': 'security/recommended',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'rules': {
-        semi: ['error', 'always'],
-        quotes: ['error', 'single'],
-        indent: ['error', 4],
-        'max-len': ['error', { code: 80 }],
-        'comma-spacing': ['error', {before: false, after: true}],
-        'node/exports-style': ['error', 'module.exports'],
-        'node/file-extension-in-import': ['error', 'always'],
-        'node/prefer-global/buffer': ['error', 'always'],
-        'node/prefer-global/console': ['error', 'always'],
-        'node/prefer-global/process': ['error', 'always'],
-        'node/prefer-global/url-search-params': ['error', 'always'],
-        'node/prefer-global/url': ['error', 'always'],
-        'node/prefer-promises/dns': 'error',
-        'node/prefer-promises/fs': 'error'        
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:security/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion:latest,
+    sourceType:'module',
+  },
+  'rules': {
+    'prettier/prettier': 'error',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'max-len': ['error', { code: 80 }],
+      'comma-spacing': ['error', {before: false, after: true}],
+      'node/exports-style': ['error', 'module.exports'],
+      'node/file-extension-in-import': ['error', 'always'],
+      'node/prefer-global/buffer': ['error', 'always'],
+      'node/prefer-global/console': ['error', 'always'],
+      'node/prefer-global/process': ['error', 'always'],
+      'node/prefer-global/url-search-params': ['error', 'always'],
+      'node/prefer-global/url': ['error', 'always'],
+      'node/prefer-promises/dns': 'error',
+      'node/prefer-promises/fs': 'error'
     }
 };
