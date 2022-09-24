44d58879cb5c5eb532b599d985bf2eab9d4f471f
tglima <thg.limadesousa@gmail.com>
2022-09-24 15:30:34 -0300

    Refatora o objeto Token

--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -3,15 +3,15 @@ const tokenConfig = require('../../config/token.config');
 const jwt = require('jsonwebtoken');
 
 exports.checkAuth = (data, callback) => {
-    const query = 
-      `SELECT IdUser FROM VW_USER_ALLOW_GET_TOKEN 
+    const query =
+      `SELECT IdUser FROM VW_USER_ALLOW_GET_TOKEN
         WHERE DeUserName = '${data.username}' and DePassword = '${data.password}';`;
-    
+
     resultDTO = {statusCode: 401, message: "Invalid credentials", success:false};
-    
-    
+
+
    db.get(query, (err, row, fields) => {
-      if (err) {        
+      if (err) {
         console.error(err);
         resultDTO = {statusCode: 500,message: "Internal Server Error", success: false};
       }
@@ -20,7 +20,7 @@ exports.checkAuth = (data, callback) => {
         const token = {};
         token.acess_token = jwt.sign({id}, tokenConfig.secret, {expiresIn: (60 * tokenConfig.minutesExpiration) });
         token.token_type = tokenConfig.tokenType;
-        token.minutesExpiration = tokenConfig.minutesExpiration;
+        token.dateTimeExpiration = new Date(+new Date() + (60000*tokenConfig.minutesExpiration));
         resultDTO = {statusCode: 200, message: "Sucess", success: true, token: token};
       }
       return callback(resultDTO);
