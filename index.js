788b351e02c8abb7539d25d5b2980d8ebbf39de8
tglima <thg.limadesousa@gmail.com>
2022-09-25 18:24:41 -0300

    Utiliza as configurações definidas em appConfig

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const app = express();
 const homeRoutes = require('./app/routes/home.routes');
 const authRoutes = require('./app/routes/auth.routes');
 const customerRoutes = require('./app/routes/customer.routes');
+const appConfig = require('./app/config/app.config');
 
 app.use(bodyParser.json());
 
-app.use('/api/v1', homeRoutes);
-app.use('/api/v1', authRoutes);
-app.use('/api/v1', customerRoutes);
+app.use(`/api/v${appConfig.api.nuVersion}`, homeRoutes);
+app.use(`/api/v${appConfig.api.nuVersion}`, authRoutes);
+app.use(`/api/v${appConfig.api.nuVersion}`, customerRoutes);
 
-app.listen(4200, () => {
-    console.log('Servidor rodando na porta: 4200');
+app.listen(`${appConfig.server.port}`, () => {
+    console.log(`Servidor rodando na porta: ${appConfig.server.port}`);
 });
