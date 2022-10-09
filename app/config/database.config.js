cea7229833d9e860ba0e108316069d05b0cea0bc
tglima <thg.limadesousa@gmail.com>
2022-10-09 11:33:15 -0300

    Alterado o tipo de acesso ao banco de dados

--- app/config/database.config.js
+++ app/config/database.config.js
@@ -1,11 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 const dbPath = (process.cwd()+'/app/assets/database/database.db');
-const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
     if(err) {
         console.error(err);
         console.error('Erro ao conectar o banco de dados!');
         return;
-    } 
+    }
     console.log('Banco de dados conectado com sucesso');
 });
 
