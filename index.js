c2ab5aa4ad5e02140fee8fcec1760eeeac194364
tglima <thg.limadesousa@gmail.com>
2022-10-10 19:10:36 -0300

    Adiciona nova rota /contact

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const homeRoutes = require('./app/routes/home.routes');
 const authRoutes = require('./app/routes/auth.routes');
 const customerRoutes = require('./app/routes/customer.routes');
+const contactRoutes = require('./app/routes/contact.routes');
 const appConfig = require('./app/config/app.config');
 
 app.use(bodyParser.json());
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(`/api/v${appConfig.api.nuVersion}`, homeRoutes);
 app.use(`/api/v${appConfig.api.nuVersion}`, authRoutes);
 app.use(`/api/v${appConfig.api.nuVersion}`, customerRoutes);
+app.use(`/api/v${appConfig.api.nuVersion}`, contactRoutes);
 
 app.listen(`${appConfig.server.port}`, () => {
     console.log(`Servidor rodando na porta: ${appConfig.server.port}`);
